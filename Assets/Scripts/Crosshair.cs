f94488939efd40915b9434a5c4bbba54ccb8fcf8
Mike <mic.bellotti02@gmail.com>
2024-06-24 16:32:39 +0200

    fix crosshair direction

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -7,16 +7,16 @@ public class Crosshair : MonoBehaviour
     [SerializeField] private SpriteRenderer _crosshairSprite;
     private void Update()
     {
-        // Vector2 crosshairScreenPos = Camera.main.WorldToScreenPoint(transform.position);
-        // Vector2 mousePos = Input.mousePosition;
-        // Vector2 screenResult = mousePos - crosshairScreenPos;
-        //
-        // float angle = Vector2.SignedAngle(Vector2.right, screenResult);
-        // Debug.Log(Vector2.SignedAngle(Vector2.right, screenResult));
-        // transform.localRotation = Quaternion.AngleAxis(-angle, Vector3.left);
+        Vector2 crosshairScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenResult = mousePos - crosshairScreenPos;
+        
+        float angle = Vector2.SignedAngle(Vector2.right, screenResult);
+        Debug.Log(Vector2.SignedAngle(Vector2.right, screenResult));
+        transform.localRotation = Quaternion.AngleAxis(transform.parent.localRotation.y > 0 ? angle : -angle + 180, Vector3.left);
 
-        Vector3 startingVector = new(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z);
-        Debug.Log(Camera.main.ScreenToWorldPoint(startingVector));
+        // Vector3 startingVector = new(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - Camera.main.transform.position.z);
+        // Debug.Log(Camera.main.ScreenToWorldPoint(startingVector));
         
         //Debug.Log($"[{gameObject.name}] Rotation: {transform.rotation}");
         
