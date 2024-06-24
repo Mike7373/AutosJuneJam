a88e5628e262d755949feb00ee4bde7a3e91c6ae
Consoletti <a@a.it>
2024-06-24 12:09:34 +0200

    wip

--- Assets/CameraFollowPlayer.cs
+++ Assets/CameraFollowPlayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
@@ -7,7 +5,6 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField] Transform target;
 
     [SerializeField] Vector3 distance;
-    
     
     void Update()
     {
