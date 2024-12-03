4f8b355b31b1ef605dacd047eed3f94b023083c0
Consoletti <a@a.it>
2024-12-03 17:34:19 +0100

    Aggiunto decal al fuoco della pistola

--- Assets/Pistol.cs
+++ Assets/Pistol.cs
@@ -1,18 +1,32 @@
 using System;
 using FMODUnity;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Pistol : MonoBehaviour
 {
     [NonSerialized]
     public Transform pistolControl;
 
-    public EventReference shootSound;       // TODO: Mettere in scriptable object
+    // TODO: Mettere in scriptable object
+    
+    public DecalProjector prefabBulletDecal;  
+    public EventReference shootSound;       
+    
+    // Fine scriptable object -----------------
+    
+    // Cose a runtime come l'ammo, non vanno nello scriptable.
+    int ammo;
     
     const float FLOAT_THRESHOLD = 0.001f;   // TODO: Mettere questa proprietà da qualche parte
+    
+    [SerializeField]
     float pistolMaxAngle = 5;               // Angolo massimo delle braccia rispetto al corpo oltre al quale il giocatore ruota (TODO: Vedere dove deve finire questa proprietà)
+    [SerializeField]
+    float decalOffset = 0.05f;              // TODO: Idem, questo resta qui?
     
     float lastAngleY;
+    
     
     public void AimTo(Vector3 target)
     {
@@ -42,7 +56,15 @@ public class Pistol : MonoBehaviour
         }
         lastAngleY = pistolControl.localRotation.eulerAngles.y;
         
-        // TODO: Gestione raycast hit e luce mirino.
+        // TODO: Gestione raycast hit e luce mirino. (Il mirino potrebbe essere un decal unlit oppure
+        /*var pistolRay = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(pistolRay, out hit))
+        {
+            // Decal
+            //luceMirino.SetActive(true);
+            //luceMirino.transform.position = hit.point-ray.direction*0.1f;
+            //luceMirino.transform.LookAt(hit.point);
+        }*/
     }
 
     
@@ -54,17 +76,14 @@ public class Pistol : MonoBehaviour
         var shootSoundInstance = RuntimeManager.CreateInstance(shootSound);
         shootSoundInstance.start();
         shootSoundInstance.release();
+        
+        var pistolRay = new Ray(pistolControl.position, pistolControl.forward);
+        if (Physics.Raycast(pistolRay, out var hit))
+        {
+            // Decal, lo istanzio leggermente dietro il punto colpito e che guarda verso il punto 
+            Instantiate(prefabBulletDecal, hit.point + decalOffset * hit.normal, Quaternion.LookRotation(-hit.normal));
+        }
     }
     
-    
-    // ============== GESTIONE FUOCO ===============
-    /*
-    var pistolRay = new Ray(transform.position, transform.forward);
-    if (Physics.Raycast(pistolRay, out hit))
-    {
-        luceMirino.SetActive(true);
-        luceMirino.transform.position = hit.point-ray.direction*0.1f;
-        luceMirino.transform.LookAt(hit.point);
-    }*/
     
 }
